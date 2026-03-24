e8fb25f4b1e107bd0f0fa6a1264e944167a1a5c8
mewo-bean <annybezrukova@gmail.com>
2026-03-24 17:24:54 +0500

    5

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -21,8 +21,42 @@ class Users {
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+<<<<<<< Updated upstream
   const users1 = new Users();
   res.send(users1.items);
+=======
+  res.send('Hello, world!');
+  users = [2]
+});
+
+class Users {
+    constructor() {
+      items =[
+          {
+            "id": 1,
+            "name": "Bezrukova Anna"
+          },
+          {
+            "id": 2,
+            "name": "Tsiganov Artyom"
+          } 
+        
+
+      ]
+    }
+
+    add(id, name) {
+      items.push(name)
+    }
+}
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  user = new Users();
+  name = req.body
+  user.
+  
+>>>>>>> Stashed changes
 });
 
 module.exports = router;
