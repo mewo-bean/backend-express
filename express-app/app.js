4a07cae27e900eb1d01d60222c869ba26dad88af
Артём Цыганов <tsiganovartem06@gmail.com>
2026-03-24 17:50:19 +0500

    7

--- express-app/app.js
+++ express-app/app.js
@@ -13,6 +13,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => 
+    {
+    if (req.query.auth != "true") {
+        res.status(401).json();
+    }
+    next();
+});
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
